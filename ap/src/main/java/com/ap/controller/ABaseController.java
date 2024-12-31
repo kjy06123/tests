c0b971481ea74b4f6769cca8740ee45c0bd2ab53
kjy06123 <kjy06123@outlook.com>
2025-01-01 06:00:25 +0800

    上传

---
+++ ap/src/main/java/com/ap/controller/ABaseController.java
@@ -0,0 +1,21 @@
+package com.ap.controller;
+
+import com.ap.enums.ResponseCodeEnum;;
+
+import com.ap.entity.vo.ResponseVO;;
+
+public class ABaseController {
+
+    protected static final String STATUC_SUCCESS = "success";
+
+    protected static final String STATUC_ERROR = "error";
+
+    protected <T> ResponseVO getSuccessResponseVO(T t) {
+        ResponseVO<T> responseVO = new ResponseVO<>();
+        responseVO.setStatus(STATUC_SUCCESS);
+        responseVO.setCode(ResponseCodeEnum.CODE_200.getCode());
+        responseVO.setInfo(ResponseCodeEnum.CODE_200.getMsg());
+        responseVO.setData(t);
+        return responseVO;
+    }
+}
